9ea5af144597f9e4cacc1fc25f9f243ee63e47cd
Jake Wiseberg <jakewiseberg@gmail.com>
2017-09-29 15:46:43 -0400

    basic omni teleop ready for testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmniDrivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmniDrivetrain.java
@@ -1,24 +1,29 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
 
 /**
  * Created by jake.wiseberg on 9/29/2017.
  */
 
+@TeleOp(name="Template: Linear OpMode", group="Linear Opmode")
+@Disabled
 public class OmniDrivetrain extends OpMode {
 
-    //values
+    //values, using global values for execution speed
     HardwareMap hMap;
     public DcMotor fr;
     public DcMotor fl;
     public DcMotor bl;
     public DcMotor br;
     private ElapsedTime runtime = new ElapsedTime();
-    double[] motorVal = new double[4];
+    double[] motorPwr = new double[4];
     double x,y,z;
 
     @Override
@@ -38,52 +43,23 @@ public class OmniDrivetrain extends OpMode {
 
     @Override
     public void loop() {
-        x = gamepad1.left_stick_x;
-        y = gamepad1.left_stick_y;
+        //getting joystick values
+        y = gamepad1.left_stick_y; //left joystick y axis
+        x = gamepad1.left_stick_x; //left joystick x axis
+        z = gamepad1.right_stick_x; //right joystick x axis
+
+        //calculating the motor power
+        motorPwr[0] = Range.clip(-y+x-z, -1.0, 1.0); //front right
+        motorPwr[1] = Range.clip(y+x-z, -1.0, 1.0); //front left
+        motorPwr[2] = Range.clip(-y-x-z, -1.0, 1.0); //back right
+        motorPwr[3] = Range.clip(y-x-z, -1.0, 1.0); //back left
+
+        //setting the motor powers
+        fr.setPower(motorPwr[0]);
+        fl.setPower(motorPwr[0]);
+        br.setPower(motorPwr[0]);
+        bl.setPower(motorPwr[0]);
 
         telemetry.addData("Status", "Run Time: " + runtime.toString());
     }
-
-/*
-    //joystick values
-    int y= joystick.joy1_y1;
-    int x= joystick.joy1_x1;
-    int z= joystick.joy1_x2;
-
-    //motor values using the variables above
-//note the negatives
-    motorTrigger(frontRight,x*.75,-y*.75,z);
-    motorTrigger(backRight,-x*.75,-y*.75,z);
-    motorTrigger(frontLeft,x*.75,y*.75,z);
-
-//other stuff maybe gotto look more into this
-    motor[backRight]=(-y-x-z)*.75;
-
-
-
-    //joystick values (translated)
-    int x= gamepad1.left_stick_x; //side to side
-    int y= gamepad1.left_stick_y; //forward/backward
-    int z= gamepad1.right_stick_x; //sideways
-
-    //motor values using the variables above
-    motor[frontLeft]=(y+x-z)*.75;
-    motor[backLeft]= (y-x-z)*.75; // -y-x||x+y
-    motor[frontRight]=(-y+x-z)*.75;//-y+x||-x+y
-
-    //__________________________
-    flPower = Range.scale( (y+x-z), -1, 1, -MAX_SPEED, MAXSPEED  ;
-
-    //note the negatives
-    motorTrigger(frontRight,x*.75,-y*.75,z);
-    motorTrigger(backRight,-x*.75,-y*.75,z);
-    motorTrigger(frontLeft,x*.75,y*.75,z);
-
-//other stuff maybe gotto look more into this
-    motor[frontLeft]=(y+x-z)*.75;
-    motor[backLeft]= (y-x-z)*.75; // -y-x||x+y
-    motor[frontRight]=(-y+x-z)*.75;//-y+x||-x+y
-    motor[backRight]=(-y-x-z)*.75;
-
-    */
 }
